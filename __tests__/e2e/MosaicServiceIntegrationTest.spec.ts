d1c65f0afc4a15cc5885ed0128d603a71739a29d
JustinasLekavicius <justinas.l@superhow.io>
2021-03-08 16:39:56 +0200

    Commented MosaicServiceIntegrationTest for the time being.

--- __tests__/e2e/MosaicServiceIntegrationTest.spec.ts
+++ __tests__/e2e/MosaicServiceIntegrationTest.spec.ts
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
  *
- */
+ *//*
 import {
     AccountInfo,
     AccountRepository,
@@ -116,3 +116,4 @@ describe('services/MosaicService', () => {
         console.log(JSON.stringify(result, null, 2));
     });
 });
+*/
